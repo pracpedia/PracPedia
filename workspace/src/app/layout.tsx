36abf06dbba9f9ead28c1625fb7e9b5eb72e5ade
Z User <z@container>
2026-08-22 17:52:38 +0000

    4f6bed2d-dbf9-4341-8804-50a5922190f3

--- workspace/src/app/layout.tsx
+++ workspace/src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
